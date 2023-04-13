e741fb00b52c526969763ca01b68c700b8c62025
FlavianMiano <flavianmiano@gmail.com>
2023-04-13 10:33:54 +0300

    Update camera.py

--- camera.py
+++ camera.py
@@ -17,10 +17,10 @@ option = st.selectbox('Select option', options)
 def import_and_predict(picture, model):
         
         #img = cv2.imread(picture, cv2.IMREAD_GRAYSCALE)  # Convert image to grayscale
-        img = img.convert('L')
+        img = picture.convert('L')
         height = 28
         width = 28
-        img = cv2.resize(picture, (width, height))  # Resize the image to 28x28
+        img = cv2.resize(img, (width, height))  # Resize the image to 28x28
             
         #size = (150,150)    
         #image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
