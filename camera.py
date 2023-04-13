96f84ebb119c3f0d100897ceb0b7f946ae04b9ac
FlavianMiano <flavianmiano@gmail.com>
2023-04-13 14:04:23 +0300

    Update camera.py

--- camera.py
+++ camera.py
@@ -5,6 +5,7 @@ import requests
 import tensorflow.keras as keras
 import urllib.request
 import cv2
+from tensorflow.keras.preprocessing.image import img_to_array
 
 
 st.title("Text to Digital Input")
@@ -30,12 +31,18 @@ elif option is 'Upload':
         picture = Image.open(picture)
         st.image(picture, use_column_width=True)
 
-#size = (28,28)
-#picture =  ImageOps.fit(picture, size, Image.ANTIALIAS)
-#picture = np.asarray(picture)
+
+picture = img_to_array(picture)
+img = np.array(picture)
+img = img / 255.0
+image = img.resize((28,28))
+
+img = img.reshape(-1, 28, 28, 1)
 
 
 model = keras.models.load_model('emnistModel.h5')
+
+predictions = model(img)
 
 # predictions = model.predict(picture)
 # st.write(predictions)
